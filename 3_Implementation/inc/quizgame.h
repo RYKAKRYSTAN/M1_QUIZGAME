f5c11529237235843de05d6037cc8782ddb5ba45
RYKAKRYSTAN <101564345+RYKAKRYSTAN@users.noreply.github.com>
2022-04-02 22:43:23 +0530

    Create quizgame.h

---
+++ 3_Implementation/inc/quizgame.h
@@ -0,0 +1,17 @@
+/**
+ * @file quizgame.h
+ * @author TEJAL R
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _quizgame_H
+#define _quizgame_H
+int  help();
+int start();
+void highscore();
+
+#endif
